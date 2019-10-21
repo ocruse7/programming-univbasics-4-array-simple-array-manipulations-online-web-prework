af23b96580a61c26d297fc18e84952905d562ff9
Owen Cruse <ocruse7@gmail.com>
2019-10-21 19:35:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,13 @@ def using_push( array, element )
 end
 
 def using_unshift( array, element )
-  
+  array.unshift(element)
+end
+
+def using_pop( array, element )
+  array.pop
+end
+
+def pop_with_args( array )
+  array.pop(2)
+END
